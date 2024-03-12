37fcbaa896316162f107341ad2f85e5f31358497
Service Web <sw@gssmail.com>
2024-03-12 12:35:35 -0500

    Commit changes in C:\Websites\Git\emarlow\4718\GCG_4718_SIM_SA_PL.sql

---
+++ GCG_4718_SIM_SA_PL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "GCG_4718_SIM_SA_PL"(
+ "Customer" CHAR(6),
+ "Part" CHAR(20),
+ "Description" CHAR(30),
+ "Product_Line" CHAR(2),
+ "Qty_Ordered" NUMERIC(13,4),
+ "Extension" NUMERIC(16,4),
+ "iBIRunID" BIGINT,
+ "Date_Order" DATE,
+ "SDate" DATE,
+ "EDate" DATE,
+ "Cost" NUMERIC(16,4),
+ "PL_Selected" LONGVARCHAR);
